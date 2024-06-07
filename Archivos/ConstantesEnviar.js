62317613af11bd0316f8c601106ab06537e9660c
Pineda-nazi <vivianafelixpineda@gmail.com>
2024-06-06 23:46:14 -0500

    Delete Archivos/ConstantesEnviar.js

--- Archivos/ConstantesEnviar.js
+++
@@ -1,9 +0,0 @@
-function ConstEnviar( vm , from , quoted , info ){
-const enviartexto = (texto) => {
- vm.sendMessage(from,{ text : texto }, {quoted : info})
- }
- }
-module.exports = {
-ConstEnviar,
-enviartexto
-}
