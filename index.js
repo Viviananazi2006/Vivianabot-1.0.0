ec0670d73609854adcee98918ea1dbf5edc978a1
Ziooo <103476365+Alexito-Hub@users.noreply.github.com>
2024-06-10 14:58:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -917,8 +917,38 @@ case 'ytmp4': case 'ytmp3': case 'play': case 'Ytmp4': case 'Ytmp3': case 'Play'
 
 case 'tiktok':
 case 'tt': {
+const tiktok = async (url) => {
+    try {
+        const tokenn = await axios.get("https://downvideo.quora-wiki.com/tiktok-video-downloader#url=" + url);
+        let a = cheerio.load(tokenn.data);
+        let token = a("#token").attr("value");
+        const param = {
+            url: url,
+            token: token,
+        };
+        const { data } = await axios.request("https://downvideo.quora-wiki.com/system/action.php", {
+                method: "post",
+                data: new URLSearchParams(Object.entries(param)),
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+                    "referer": "https://downvideo.quora-wiki.com/tiktok-video-downloader",
+                },
+            }
+        );
+        return {
+            status: 200,
+            title: data.title,
+            thumbnail: "https:" + data.thumbnail,
+            duration: data.duration,
+            media: data.medias,
+        };
+    } catch (e) {
+        return e
+    }
+}
     if (deviceType === 'Android') {
-        vm.sendMessage(from, { text: ' hi' })
+        vm.sendMessage(from, { video: {url: tiktok.media} })
         
     } else if (!deviceType === 'Android') {
         
